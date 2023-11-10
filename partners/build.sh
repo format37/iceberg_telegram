0f3910423cae2691b51bfa5c20b34addfa532769
Alex <format37@gmail.com>
2023-11-10 13:34:20 +0400

    modified:   build.sh 	modified:   run.sh

--- partners/build.sh
+++ partners/build.sh
@@ -1,1 +1,1 @@
-docker build -t partners_bot .
+sudo docker build -t partners_bot .
--- partners/run.sh
+++ partners/run.sh
@@ -1,2 +1,2 @@
-# docker run -d --restart always -p 7401:7401  partners_bot
-docker run -p 7401:7401  partners_bot
+# sudo docker run -d --restart always -p 7401:7401  partners_bot
+sudo docker run -p 7401:7401  partners_bot
