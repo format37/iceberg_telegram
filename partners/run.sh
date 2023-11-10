b4f1c7a8200d225340700ab03505fffb4abe837a
Alex <format37@gmail.com>
2023-11-10 13:41:31 +0400

    modified:   run.sh

--- partners/run.sh
+++ partners/run.sh
@@ -1,2 +1,3 @@
-sudo docker run -d --restart always -p 7401:7401  partners_bot --name partners_bot
+# sudo docker run -d --restart always -p 7401:7401  partners_bot --name partners_bot
+sudo docker run -d --restart always --name partners_bot -p 7401:7401 partners_bot
 # sudo docker run -p 7401:7401  partners_bot
