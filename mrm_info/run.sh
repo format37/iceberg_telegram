a5a5d501203d2da152be885f49a6a5301ee1477b
Alex <format37@gmail.com>
2023-11-13 13:30:24 +0400

    modified:   mrm_info/run.sh

--- mrm_info/run.sh
+++ mrm_info/run.sh
@@ -1,9 +1,14 @@
 # sudo docker run -d --restart always --name mrm_info_bot -p 7402:7402 mrm_info_bot
 # sudo docker run -p 7402:7402  mrm_info_bot
+# stop container
+sudo docker stop mrm_info_bot
+# remove container
+sudo docker rm mrm_info_bot
 sudo docker run \
     -d \
     --restart always \
     -p 7402:7402 \
+    --name mrm_info_bot \
     -e MRMSUPPORTBOT_AUTH_LOGIN="login" \
     -e MRMSUPPORTBOT_AUTH_PASSWORD="password" \
     mrm_info_bot
