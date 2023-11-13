948bee59b70614ac746e32289d72e9f9237c833a
Alex <format37@gmail.com>
2023-11-13 12:42:00 +0400

    modified:   mrm_info/run.sh 	modified:   mrm_info/server.py

--- mrm_info/run.sh
+++ mrm_info/run.sh
@@ -1,2 +1,7 @@
 # sudo docker run -d --restart always --name mrm_info_bot -p 7402:7402 mrm_info_bot
-sudo docker run -p 7402:7402  mrm_info_bot
+# sudo docker run -p 7402:7402  mrm_info_bot
+sudo docker run \
+    -p 7402:7402 \
+    -e MRMSUPPORTBOT_AUTH_LOGIN="login" \
+    -e MRMSUPPORTBOT_AUTH_PASSWORD="password" \
+    mrm_info_bot
