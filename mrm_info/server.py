3c5d58dc8d90fd245d40836e2ef6d0acf3c3e8f2
Alex <format37@gmail.com>
2024-02-13 15:03:29 +0400

    modified:   mrm_info/server.py

--- mrm_info/server.py
+++ mrm_info/server.py
@@ -95,7 +95,7 @@ async def call_message(request: Request):
 
     answer = "Система временно находится на техническом обслуживании. Приносим извенение за доставленные неудобства."
 
-    if str(message['chat']['id']) in granted_chats:
+    if str(message['chat']['id']) in granted_chats and 'forward_from' in message:
         logger.info(str(message['chat']['id'])+' in granted_chats')
         # if message.forward_from is not None:
         if 'forward_from' in message:
