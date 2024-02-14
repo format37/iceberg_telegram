58b53883ce5d969d7f6699507c72c0bbfffc0823
Alex <format37@gmail.com>
2024-02-14 10:10:33 +0400

    new file:   mrm_info_test/Dockerfile 	new file:   mrm_info_test/archive/server.py 	new file:   mrm_info_test/build.sh 	new file:   mrm_info_test/logs.sh 	new file:   mrm_info_test/requirements.txt 	new file:   mrm_info_test/run.sh 	new file:   mrm_info_test/server.py

---
+++ mrm_info_test/run.sh
@@ -0,0 +1,13 @@
+# stop container
+sudo docker stop mrm_info_bot_test
+# remove container
+sudo docker rm mrm_info_bot_test
+# run container
+sudo docker run \
+    -d \
+    --restart always \
+    -p 7403:7403 \
+    --name mrm_info_bot_test \
+    -e MRMSUPPORTBOT_AUTH_LOGIN="login" \
+    -e MRMSUPPORTBOT_AUTH_PASSWORD="password" \
+    mrm_info_bot_test
---
+++ mrm_info_test/server.py
@@ -0,0 +1,128 @@
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+import os
+import logging
+from requests.auth import HTTPBasicAuth  # or HTTPDigestAuth, or OAuth1, etc.
+from requests import Session
+from zeep import Client
+from zeep.transports import Transport
+
+# Initialize FastAPI
+app = FastAPI()
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+@app.get("/test")
+async def call_test():
+    logger.info('call_test')
+    return JSONResponse(content={"status": "ok"})
+
+
+def mrmsupport_bot_user_info(user_id, clientPath):
+    login = os.environ.get('MRMSUPPORTBOT_AUTH_LOGIN', '')
+    password = os.environ.get('MRMSUPPORTBOT_AUTH_PASSWORD', '')
+
+    session = Session()
+    session.auth = HTTPBasicAuth(login, password)
+
+    results = []
+    for w in clientPath:
+        client = Client(w, transport=Transport(session=session))
+        logger.info('Calling user_info from: ' + str(w))
+        try:
+            res = client.service.user_info(user_id, '')
+            logger.info('user_info result: ' + str(res))
+            # code		= res.result.code
+            # message		= res.result.message
+            if res and res['result']:
+                results.append(str(res))
+        except Exception as e:
+            logger.error(str(w) + ' user_info error: ' + str(e))
+    logger.info('user_info results count: ' + str(len(results)))
+    return results
+
+
+@app.post("/message")
+async def call_message(request: Request):
+    logger.info('call_message')
+    message = await request.json()
+    logger.info(message)
+    """
+    {
+        'message_id': 89, 
+        'from': {
+            'id': 106129214, 
+            'is_bot': False, 
+            'first_name': 'Alex', 
+            'username': 'format37', 
+            'language_code': 'en', 
+            'is_premium': True
+        }, 
+        'chat': {
+            'id': -1001533625926, 
+            'title': 'Bot factory 2', 
+            'type': 'supergroup'
+        }, 
+        'date': 1699863766, 
+        'forward_from': {
+            'id': 106129214, 
+            'is_bot': False, 
+            'first_name': 'Alex', 
+            'username': 'format37', 
+            'language_code': 'en', 
+            'is_premium': True
+        }, 
+        'forward_date': 1678700186, 
+        'text': 'hello'
+    }
+    """
+
+    clientPath = [
+        'http://10.2.4.123/productionMSK/ws/Telegram.1cws?wsdl',
+        'http://10.2.4.123/productionNNOV/ws/Telegram.1cws?wsdl',
+        'http://10.2.4.123/productionSPB/ws/Telegram.1cws?wsdl'
+    ]
+
+    granted_chats = [
+        '-1001853379941', # MRM master info МРМ мастер, 
+        '-1002094333974', # botchat (test)
+        '-1002087087929' # TehPodMRM Comments
+    ]
+
+    answer = "Система временно находится на техническом обслуживании. Приносим извенение за доставленные неудобства."
+
+    if str(message['chat']['id']) in granted_chats and 'forward_origin' in message:
+        logger.info(str(message['chat']['id'])+' in granted_chats')
+        # if message.forward_from is not None:
+        if 'forward_from' in message:
+            # logger.info('Received redirect from user id: '+str(message.forward_from.id))
+            logger.info('Received redirect from user id: '+str(message['forward_from']['id']))
+            reply = '[\n'
+            # results = mrmsupport_bot_user_info(message.forward_from.id, clientPath)
+            results = mrmsupport_bot_user_info(message['forward_from']['id'], clientPath)
+            
+            if len(results) == 0:
+                answer = 'User not found'
+                logger.info(answer)
+            else:
+                reply += ',\n'.join(results)
+                answer = reply + '\n]'
+                # logger.info('Replying in '+str(message.chat.id))
+                logger.info('Replying in '+str(message['chat']['id']))
+        else:
+            answer = 'Unable to retrieve the hidden user id'
+            logger.info(answer)
+    else:
+        return JSONResponse(content={
+                "type": "empty",
+                "body": ""
+                })
+
+    return JSONResponse(content={
+        "type": "text",
+        "body": str(answer)
+        })
