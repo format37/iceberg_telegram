ddc2e1d6ecf27729b3561ba8f08580a56d4e0707
Alex <format37@gmail.com>
2024-02-20 10:05:18 +0400

    modified:   mrm_info/Dockerfile 	new file:   mrm_info/data/app_doc.txt 	new file:   mrm_info/data/qa.json 	new file:   mrm_info/data/support.txt 	new file:   mrm_info/extract.py 	modified:   mrm_info/logs.sh 	modified:   mrm_info/requirements.txt 	modified:   mrm_info/run.sh 	modified:   mrm_info/server.py 	new file:   mrm_info/source/result.json

--- mrm_info/run.sh
+++ mrm_info/run.sh
@@ -1,14 +1,13 @@
-# sudo docker run -d --restart always --name mrm_info_bot -p 7402:7402 mrm_info_bot
-# sudo docker run -p 7402:7402  mrm_info_bot
 # stop container
-sudo docker stop mrm_info_bot
+sudo docker stop mrm_info_bot_test
 # remove container
-sudo docker rm mrm_info_bot
+sudo docker rm mrm_info_bot_test
+# run container
 sudo docker run \
     -d \
     --restart always \
-    -p 7402:7402 \
-    --name mrm_info_bot \
+    -p 7403:7403 \
+    --name mrm_info_bot_test \
     -e MRMSUPPORTBOT_AUTH_LOGIN="login" \
     -e MRMSUPPORTBOT_AUTH_PASSWORD="password" \
     mrm_info_bot
--- mrm_info/server.py
+++ mrm_info/server.py
@@ -7,6 +7,32 @@ from requests import Session
 from zeep import Client
 from zeep.transports import Transport
 
+import json
+import requests
+# import time
+import glob
+import json
+from pydantic import BaseModel, Field
+from langchain.agents import Tool, initialize_agent
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_community.tools import StructuredTool
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+# from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import DuckDuckGoSearchResults
+from langchain.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.chains import RetrievalQA
+from langchain.tools import Tool
+# from langchain.schema import TextOutput
+from langchain_experimental.utilities import PythonREPL
+import time as py_time
+from pathlib import Path
+import tiktoken
+
+
 # Initialize FastAPI
 app = FastAPI()
 
@@ -15,6 +41,112 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+class DocumentProcessor:
+    def __init__(self, context_path):
+        self.context_path = context_path
+
+    def process_documents(self):
+        context_path = self.context_path
+        logger.info(f"Processing documents from {context_path}")
+        loader = DirectoryLoader(context_path, glob="*", loader_cls=TextLoader)
+        docs = loader.load()
+        logger.info(f"Loaded {len(docs)} documents")
+        api_key = os.environ.get('OPENAI_API_KEY', '')
+        embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+        text_splitter = RecursiveCharacterTextSplitter()
+        documents = text_splitter.split_documents(docs)
+        logger.info(f"Split {len(documents)} documents")
+        vector = DocArrayInMemorySearch.from_documents(documents, embeddings)
+        return vector.as_retriever()
+    
+document_processor = DocumentProcessor(context_path='/server/data/')
+retriever = document_processor.process_documents()
+
+class TextOutput(BaseModel):
+    text: str = Field(description="Text output")
+
+class BotActionType(BaseModel):
+    val: str = Field(description="Tool parameter value")
+
+class DocumentInput(BaseModel):
+    question: str = Field()
+
+class ChatAgent:
+    def __init__(self, retriever):
+        # Initialize logging
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        # self.config = bot_instance.config
+        self.config = {
+            'model': 'gpt-4-0125-preview',
+            # 'model': 'gpt-3.5-turbo',
+            'temperature': 0.7,
+        }
+        self.retriever = retriever
+        # self.bot_instance = bot_instance  # Passing the Bot instance to the ChatAgent
+        # self.logger.info(f"ChatAgent function: {self.bot_instance.bot_action_come}")
+        self.agent = self.initialize_agent()
+        
+
+    def initialize_agent(self):
+        llm = ChatOpenAI(
+            openai_api_key=os.environ.get('OPENAI_API_KEY', ''),
+            model=self.config['model'],
+            temperature=self.config['temperature'],
+        )
+        tools = []
+        # python_repl = PythonREPL()
+        # Non direct return
+        """repl_tool = Tool(
+            name="python_repl",
+            description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`.",
+            func=python_repl.run,
+            return_direct=False,
+        )
+        tools.append(repl_tool)"""
+        """tools = [self.create_structured_tool(func, name, description, return_direct)
+                 for func, name, description, return_direct in [
+                        (self.bot_instance.web_browser_tool, "Web browsing",
+                            "Provide a link to request", True)
+                      ]
+                 ]"""
+        # embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get('OPENAI_API_KEY', ''))
+        # web_browsing_tool = SimulatedWebBrowsingTool(llm, embeddings)
+        # tools.append(web_browsing_tool)
+        # tools.append(DuckDuckGoSearchRun())
+        # tools.append(DuckDuckGoSearchResults())
+        # wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+        # tools.append(wikipedia)
+        tools.append(
+            Tool(
+                args_schema=DocumentInput,
+                name='Knowledge base',
+                description="Providing a game information from the knowledge base",
+                func=RetrievalQA.from_chain_type(llm=llm, retriever=self.retriever),
+            )
+        )
+        return initialize_agent(
+            tools,
+            llm,
+            agent='chat-conversational-react-description',
+            verbose=True,
+            handle_parsing_errors=True
+        )
+    
+
+
+    @staticmethod
+    def create_structured_tool(func, name, description, return_direct):
+        print(f"create_structured_tool name: {name} func: {func}")
+        return StructuredTool.from_function(
+            func=func,
+            name=name,
+            description=description,
+            args_schema=BotActionType,
+            return_direct=return_direct,
+        )
+
 
 @app.get("/test")
 async def call_test():
@@ -22,28 +154,22 @@ async def call_test():
     return JSONResponse(content={"status": "ok"})
 
 
-def mrmsupport_bot_user_info(user_id, clientPath):
-    login = os.environ.get('MRMSUPPORTBOT_AUTH_LOGIN', '')
-    password = os.environ.get('MRMSUPPORTBOT_AUTH_PASSWORD', '')
-
-    session = Session()
-    session.auth = HTTPBasicAuth(login, password)
-
-    results = []
-    for w in clientPath:
-        client = Client(w, transport=Transport(session=session))
-        logger.info('Calling user_info from: ' + str(w))
-        try:
-            res = client.service.user_info(user_id, '')
-            logger.info('user_info result: ' + str(res))
-            # code		= res.result.code
-            # message		= res.result.message
-            if res and res['result']:
-                results.append(str(res))
-        except Exception as e:
-            logger.error(str(w) + ' user_info error: ' + str(e))
-    logger.info('user_info results count: ' + str(len(results)))
-    return results
+def mrmsupport_bot_user_info(user_id):
+    # Server base URL
+    base_url = "https://service.icecorp.ru:7403"
+    url = f"{base_url}/user_info"
+    headers = {'Content-Type': 'application/json'}
+    token = os.environ.get('MRMSUPPORTBOT_TOKEN', '')
+    data = {
+        'token': token,
+        'user_id': user_id
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    logger.info(f"User Info Endpoint Response: {response}")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
 
 
 @app.post("/message")
@@ -81,12 +207,6 @@ async def call_message(request: Request):
     }
     """
 
-    clientPath = [
-        'http://10.2.4.123/productionMSK/ws/Telegram.1cws?wsdl',
-        'http://10.2.4.123/productionNNOV/ws/Telegram.1cws?wsdl',
-        'http://10.2.4.123/productionSPB/ws/Telegram.1cws?wsdl'
-    ]
-
     granted_chats = [
         '-1001853379941', # MRM master info МРМ мастер, 
         '-1002094333974', # botchat (test)
@@ -97,25 +217,58 @@ async def call_message(request: Request):
 
     if str(message['chat']['id']) in granted_chats and 'forward_origin' in message:
         logger.info(str(message['chat']['id'])+' in granted_chats')
-        # if message.forward_from is not None:
+        results = []
         if 'forward_from' in message:
-            # logger.info('Received redirect from user id: '+str(message.forward_from.id))
             logger.info('Received redirect from user id: '+str(message['forward_from']['id']))
             reply = '[\n'
-            # results = mrmsupport_bot_user_info(message.forward_from.id, clientPath)
-            results = mrmsupport_bot_user_info(message['forward_from']['id'], clientPath)
-            
-            if len(results) == 0:
-                answer = 'User not found'
-                logger.info(answer)
-            else:
-                reply += ',\n'.join(results)
-                answer = reply + '\n]'
-                # logger.info('Replying in '+str(message.chat.id))
-                logger.info('Replying in '+str(message['chat']['id']))
+            results = mrmsupport_bot_user_info(message['forward_from']['id'])
         else:
-            answer = 'Unable to retrieve the hidden user id'
+            results.append('User not found')
+
+        # Get the Langchain LLM opinion
+        chat_history = []
+        # retriever = None
+        # document_processor = DocumentProcessor(context_path='/server/data/')
+        # retriever = document_processor.process_documents()
+        
+        message_text = f"""Получен запрос на техническую поддержку от пользователя мобильного приложения: "{message['text']}"
+Техническая информация о пользователе:\n"""
+        message_text += str(results) if len(results) > 0 else "Не предоставлена" # Tech info from 1C
+        message_text += """\n
+Пожалуйста, обратитесь к вашей базе знаний и предоставьте ответ на Русском языке в формате JSON в строгом соответствии с шаблоном: 
+{
+"Возможные причины":"Описание возможных причин",
+
+"Ответ пользователю":"Ответ который получит пользователь",
+
+"Комментарий разработчику":"Комментарий который получат разработчики. Пустая строка если комментарий не нужен."
+}"""
+        message_text = message_text.replace('\n', ' ')
+        chat_agent = ChatAgent(retriever)
+        response = chat_agent.agent.run(
+            input=message_text, 
+            chat_history=chat_history
+        )
+        # logger.info(f"ChatAgent response: {response}")
+        results.append(response)
+        
+        """if len(results) == 0:
+            answer = 'User not found'
             logger.info(answer)
+        else:"""
+        
+        # reply += ',\n'.join(results)
+        # Before joining the results, convert each item to a string if it's not already one
+        results_as_strings = [json.dumps(item) if isinstance(item, dict) else str(item) for item in results]
+        # Now you can safely join the string representations of your results
+        reply += ',\n'.join(results_as_strings)  # This line replaces line 230 in your code
+        
+        answer = reply + '\n]'
+        # logger.info('Replying in '+str(message.chat.id))
+        logger.info('Replying in '+str(message['chat']['id']))
+        """else:
+            answer = 'Unable to retrieve the hidden user id'
+            logger.info(answer)"""
     else:
         return JSONResponse(content={
                 "type": "empty",
