d1109c38d81e7ea8001c45aaebb241b8d6509a6b
Alex <format37@gmail.com>
2024-02-20 15:01:00 +0400

    modified:   run.sh 	modified:   test_run.sh

--- mrm_info/run.sh
+++ mrm_info/run.sh
@@ -1,13 +1,20 @@
 # stop container
-sudo docker stop mrm_info_bot_test
+sudo docker stop mrm_info_bot
 # remove container
-sudo docker rm mrm_info_bot_test
+sudo docker rm mrm_info_bot
 # run container
 sudo docker run \
     -d \
     --restart always \
-    -p 7403:7403 \
-    --name mrm_info_bot_test \
-    -e MRMSUPPORTBOT_AUTH_LOGIN="login" \
-    -e MRMSUPPORTBOT_AUTH_PASSWORD="password" \
+    -p 7402:7402 \
+    --name mrm_info_bot \
+    -e MRMSUPPORTBOT_AUTH_LOGIN="" \
+    -e MRMSUPPORTBOT_AUTH_PASSWORD="" \
+    -e OPENAI_API_KEY= \
+    -e LANGCHAIN_TRACING_V2=true \
+    -e LANGCHAIN_ENDPOINT=https://api.smith.langchain.com \
+    -e LANGCHAIN_API_KEY= \
+    -e LANGCHAIN_PROJECT="mrm_info" \
+    -e MRMSUPPORTBOT_TOKEN="" \
+    -v ./data:/server/data \
     mrm_info_bot
