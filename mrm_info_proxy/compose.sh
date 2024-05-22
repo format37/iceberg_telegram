bd97741eed04fe5cddfdc80cd7dbadb8b9a549ab
Alex <format37@gmail.com>
2024-05-22 14:54:44 +0400

    modified:   compose.sh

--- mrm_info_proxy/compose.sh
+++ mrm_info_proxy/compose.sh
@@ -1,3 +1,3 @@
 sudo docker compose down -v
-# sudo docker compose up --build --force-recreate --remove-orphans -d
-sudo docker compose up --build --force-recreate --remove-orphans
+sudo docker compose up --build --force-recreate --remove-orphans -d
+# sudo docker compose up --build --force-recreate --remove-orphans
